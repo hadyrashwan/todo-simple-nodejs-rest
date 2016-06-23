20eb5262cb002845c9535038cd5f57948c312535
Hady Rashwan <h2rashwan@gmail.com>
2016-06-24 00:03:07 +0200

    validation for empty string and only the required fields will be takes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var bodyParser=require('body-parser')
+var _=require('underscore')
 var app=express()
 var PORT= process.env.PORT || 3000
 app.use(bodyParser.json())
@@ -7,27 +8,39 @@ app.use(bodyParser.json())
 var todos=[]
 var todoIdNext=1
 
-app.get('/todos/:id',function(req,res){
-	//res.send('todos id is : ' +req.params.id)
-	//res.json(todos[req.params.id]) try with just the index
-	todos.forEach(function(todo){
-		if(todo.id==req.params.id)
-			res.json(todo)
-	})
-	res.status(404).send()
-
-})
 
 app.get('/',function(req,res){
 	res.send("the todo thing")
 })
+
+app.get('/todos/:id',function(req,res){
+	//res.send('todos id is : ' +req.params.id)
+	//res.json(todos[req.params.id]) try with just the index
+	todoId=parseInt(req.params.id,10)
+	var isFound=_.findWhere(todos,{id:todoId })
+	if(isFound){
+			res.json(isFound)
+	}else{
+	res.status(404).send()
+}
+
+})
+
+
 
 app.get('/todos',function(req,res){
 	res.json(todos)
 })
 
 app.post('/todos',function(req,res){
-	var todoItem=req.body
+	var body=req.body
+	body=_.pick(body,"description", "completed")
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ){
+		return res.status(400).send();
+	}
+
+	body.description=body.description.trim()
+	var todoItem=body
 	todoItem.id=todoIdNext
 	todos.push(todoItem)
 	console.log(todos[todoIdNext-1])
