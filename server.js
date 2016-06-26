23d791e7674f0fc3182daf5a9a01609e1fefcdd0
Hady Rashwan <h2rashwan@gmail.com>
2016-06-26 13:01:04 +0200

    all the msgs are working with tokens now

--- server.js
+++ server.js
@@ -4,43 +4,37 @@ var _ = require('underscore')
 var app = express()
 var PORT = process.env.PORT || 3000
 db = require('./db.js')
-middleware=require('./middleware.js')(db)
+middleware = require('./middleware.js')(db)
 app.use(bodyParser.json())
 var todos = []
 var todoIdNext = 1
 
 
 app.get('/', function(req, res) {
-    return res.json(todos)
+    return res.send("WELCOME TO HODZ->TODO-simple-Node.js")
 
 })
 
-app.get('/todos/:id',middleware.requireAuthentication, function(req, res) { // findy by Id function
+app.get('/todos/:id', middleware.requireAuthentication, function(req, res) { // findy by Id function
 
     todoId = parseInt(req.params.id, 10)
-    db.todo.findById(todoId).then(function(todo) {
-        if (todo) {
-            console.log(todo.toJSON())
-            res.json(todo)
-        } else {
-            console.log('todo with this id  wasnt found')
-            res.status(404).send();
+    req.user.getTodos({
+        where: {
+            id: todoId
         }
-
+    }).then(function(todo) {
+        if (todo)
+            return res.json(todo)
+        else
+            return res.status(404).send()
     }, function(e) {
-        console.error(e);
-        res.status(500).send(e);
-
-    }).catch(function(e) {
-        res.status(500).send(e)
-        console.error(e);
+        res.status(404).send("item isn't found")
     })
-
 })
 
 
 
-app.get('/todos',middleware.requireAuthentication, function(req, res) { // Search function
+app.get('/todos', middleware.requireAuthentication, function(req, res) { // Search function
     var queryPram = req.query;
     where = {}
 
@@ -54,51 +48,47 @@ app.get('/todos',middleware.requireAuthentication, function(req, res) { // Searc
         }
     }
 
-    db.todo.findAll({
+    req.user.getTodos({
         where: where
     }).then(function(todos) {
-        if (todos)
+        if (todos.length > 0)
             res.json(todos)
         else
             res.status(404).send("no item found")
     }, function(e) {
         res.status(500).send(e);
     })
+
+
 })
 
-app.delete('/todos/:id', function(req, res) { // del by Id function
-    var todoId2 = parseInt(req.params.id, 10)
+app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) { // del by Id function
+    var todoId = parseInt(req.params.id, 10)
 
+            db.todo.destroy({
+            where: {
+                id: todoId,
+                userId:req.user.id
+            }
+        }).then(function(todo) {
+            res.send("Item has been deleted")
+        }, function() {
+            res.status(500).send();
+        })
 
-    db.todo.destroy({
-        where: {
-            id: todoId2
-        }
-    }).then(function(numberOfItems) {
-        if (numberOfItems === 0)
-            return res.status(404).send('the id isnt found')
-        res.send(numberOfItems + " items has been  deleted!")
-    }, function(e) {
-        console.log(e)
-        res.status(404).send(e)
-    }).catch(function(e) {
-        console.log(e)
-        res.status(500).send(e)
-    })
 })
 
-app.post('/todos',middleware.requireAuthentication, function(req, res) { // add todo function
+app.post('/todos', middleware.requireAuthentication, function(req, res) { // add todo function
     var body = req.body
     body = _.pick(body, "description", "completed")
 
 
     db.todo.create(body).then(function(todo) {
-    	req.user.addTodo(todo).then(function(){
-    		console.log("im in")
-    		return todo.reload();
-    	}).then(function(todo){
-    		res.send(todo.toJSON())
-    	})
+        req.user.addTodo(todo).then(function() {
+            return todo.reload();
+        }).then(function(todo) {
+            res.send(todo.toJSON())
+        })
     }, function() {
         res.status(400).json(e);
     })
@@ -107,7 +97,7 @@ app.post('/todos',middleware.requireAuthentication, function(req, res) { // add 
 })
 
 
-app.put('/todos/:id',middleware.requireAuthentication, function(req, res) { // update function
+app.put('/todos/:id', middleware.requireAuthentication, function(req, res) { // update function
     var body = req.body
     var todoId = parseInt(req.params.id, 10)
 
@@ -123,14 +113,19 @@ app.put('/todos/:id',middleware.requireAuthentication, function(req, res) { // u
         attributes.description = body.description
 
 
-    db.todo.findById(todoId).then(function(todo) {
+    db.todo.findOne({
+        where: {
+            id: todoId,
+            userId:req.user.id
+        }
+    }).then(function(todo) {
 
         if (todo)
             return todo.update(attributes).then(function(todo) {
-        res.json(todo.toJSON());
-    }, function(e) {
-        res.status(400).send(e);
-    })
+                res.json(todo.toJSON());
+            }, function(e) {
+                res.status(400).send(e);
+            })
 
         else
             res.status(404).send();
@@ -139,8 +134,8 @@ app.put('/todos/:id',middleware.requireAuthentication, function(req, res) { // u
     })
 })
 
-app.post('/users',function(req,res){ // create account function
-	var body = req.body
+app.post('/users', function(req, res) { // create account function
+    var body = req.body
     body = _.pick(body, "email", "password")
 
     db.user.create(body).then(function(newUser) {
@@ -153,28 +148,28 @@ app.post('/users',function(req,res){ // create account function
 })
 
 
-app.post('/users/login',function(req,res){ // login function
-	var body = req.body
+app.post('/users/login', function(req, res) { // login function
+    var body = req.body
     body = _.pick(body, "email", "password")
 
-    db.user.authenticate(body).then(function(userData){
-    	 token =userData.generateToken('authentication')
-    	if(token){
-    		res.header('Auth',token).json(userData.toPublicJson());
-    	}else
-    	res.status(500).send();
-	    	//res.json(userData.toPublicJson());
-    },function(e){
-    	console.error(e)
-    	res.status(401).send();
+    db.user.authenticate(body).then(function(userData) {
+        token = userData.generateToken('authentication')
+        if (token) {
+            res.header('Auth', token).json(userData.toPublicJson());
+        } else
+            res.status(500).send();
+        //res.json(userData.toPublicJson());
+    }, function(e) {
+        console.error(e)
+        res.status(401).send();
 
     })
-	
+
 })
 
 db.sequelize.sync(
-	{force:true}
-	)
+    //{force:true} this restarts the server for development option DON'T DO IT IN PRODUCTION
+)
 app.listen(PORT, function() {
     console.log('running on port : ', PORT)
 })
